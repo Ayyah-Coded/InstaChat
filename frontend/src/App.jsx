a672d9ffcf944c8ed17387f15d5506913fddd803
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-08-16 11:23:08 +0100

    - integrated Clerk for authentication and enhance theme management - Implemented theme and wallpaper contexts for managing themes and wallpapers. - Developed AuthPage and ChatPage with structured layouts and integrated new components. - Added new utility functions for theme and wallpaper management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,45 @@
-
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import { useAuthStore } from "./store/useAuthStore";
+import PageLoader from "./components/PageLoader";
+import { Toaster } from "react-hot-toast";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import { useEffect } from "react";
 
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
 
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
-    <h1>InstaChat</h1>
-  )
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
+  );
 };
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { ClerkProvider } from "@clerk/react";
+import { BrowserRouter } from "react-router";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
-)
+);
